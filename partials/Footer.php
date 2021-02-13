bd02bef2b0d0af4c2ccaa488d3dac7f4285573c9
Mori Hetarthi <71168600+MoriHetarthi@users.noreply.github.com>
2021-02-13 17:27:13 +0530

    Add files via upload

---
+++ partials/Footer.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <style>
+    .container-fluid {
+        margin-top: 400px;
+    }
+    </style>
+</head>
+
+<body>
+
+    <div class="container-fluid bg-dark text-light">
+        <p class="text-center py-3 mb-0">Discussed coding techniques. Designed by Mori Hetarthi. All rights reserver.
+            Copyright
+            2020</p>
+    </div>
+</body>
+
+</html>
